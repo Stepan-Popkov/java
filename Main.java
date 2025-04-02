fb1dbfdd4c4ebc0f1477f7934463a2a3f56c656b
Stepan-Popkov <stepanpopkov1099@gmail.com>
2025-04-02 19:03:32 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,6 +24,14 @@ abstract class Animal extends LifeForm{
     void grow(){
         System.out.println("Animal "+name+ " became older");//я решил что так будет лучше
     }
+       public Animal(){
+        this.name="Unknown";
+        this.age=0;
+    }
+    public Animal (String name, int age){
+        this.name=name;
+        this.age=age;
+    }
 }
 abstract class Plant extends LifeForm{
     abstract void performPhotosynthesis();
@@ -34,6 +42,14 @@ abstract class Plant extends LifeForm{
     @Override
     void grow(){
         System.out.println("Plant "+ name + " is growing towards the sun and height now is: "+ height);
+    }
+     public Plant(){
+        this.name="Unknown";
+        this.age=0;
+    }
+    public Plant (String name, int age){
+        this.name=name;
+        this.age=age;
     }
 
 }
@@ -61,6 +77,14 @@ class Dog extends Animal implements CanRun{
    public void run(){
         System.out.println("Dog "+ name+ " is running");
     }
+    public Dog(){
+        this.name="Unknown";
+        this.age=0;
+    }
+    public Dog (String name, int age){
+        this.name=name;
+        this.age=age;
+    }
 
 }
 class Cat extends Animal implements CanRun{
@@ -71,6 +95,14 @@ class Cat extends Animal implements CanRun{
     @Override
     public void run(){
         System.out.println("Cat "+ name+ " is running");
+    }
+     public Cat(){
+        this.name="Unknown";
+        this.age=0;
+    }
+    public Cat (String name, int age){
+        this.name=name;
+        this.age=age;
     }
 
 }
@@ -83,6 +115,15 @@ class Duck extends Animal implements CanFly, CanSwim{
     public void fly(){System.out.println("Duck "+name+" is flying");}
     @Override
     public void swim(){System.out.println("Duck "+name+" is swimming");}
+    public Duck(){
+        this.name="Unknown";
+        this.age=0;
+    }
+    public Duck (String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+
 }
 class Rose extends Plant implements HasThorns {
     @Override
@@ -94,8 +135,34 @@ class Rose extends Plant implements HasThorns {
     void performPhotosynthesis() {
         System.out.println("Plant " + name + " is performing photosynthesis");
     }
+     public Rose(){
+        this.name="Unknown";
+        this.age=0;
+    }
+    public Rose (String name, int age){
+        this.name=name;
+        this.age=age;
+    }
 }
+class AppleTree extends Plant implements ProducesFruit{
+    @Override
+    public void performPhotosynthesis(){
+        System.out.println("Plant "+name+" is performing photosynthesis");
+    }
+    @Override
+    public void produceFruit(){
+        System.out.println("AppleTree "+ name+" produces apples");
+    }
+    public AppleTree(){
+        this.name="Unknown";
+        this.age=0;
+    }
+    public AppleTree(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
 
+}
 
 
 
@@ -107,6 +174,26 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         List<LifeForm> Forms=new ArrayList<>();
         Forms.add(new Cat("Tigr", 9));
+          Forms.add(new AppleTree("Red Apples", 7));
+        Forms.add(new Duck("Donald", 14));
+        Forms.add(new Dog("Moly",2));
+        Forms.add(new Rose("White", 4));
+        
+
+        for (int i = 0; i < Forms.size(); i++) {
+            Forms.get(i).grow();
+        }
+          for (int k=0; k< Forms.size();k++){
+              if(Forms.get(k) instanceof Animal){
+                  ((Animal) Forms.get(k)).makeSound();
+                  ((Animal) Forms.get(k)).eat();
+              }
+
+
+              else if(Forms.get(k) instanceof Plant) {
+                  Forms.get(k).absorbWater();
+              }
+          }
 
 
         for (int i = 1; i <= 5; i++) {
